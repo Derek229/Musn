fa76d40514aab4d4fc75d4da27dcc509c12b4840
Trevor von Hake <tvonhake@outlook.com>
2021-04-09 16:45:07 -0600

    working on navbar update and user ui

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AuthConsumer, } from "../providers/AuthProvider";
-import { Menu, } from 'semantic-ui-react'
+import { Navbar } from 'react-bootstrap'
 import { Link, withRouter, } from 'react-router-dom'
 
 class Navbar extends React.Component {
@@ -38,24 +38,47 @@ class Navbar extends React.Component {
       )
     }
   }
+
+  
   
   render() {
     return (
       <div>
-        <Menu pointing secondary>
-          <Link to='/'>
-            <Menu.Item
-              name='home'
-              id='home'
-              active={this.props.location.pathname === '/'}
-            />
-          </Link>
-            { this.rightNavItems() }
-        </Menu>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Musn</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+          </Navbar>
       </div>
     )
   }
 }
+
+// export class ConnectedNavbar extends React.Component {
+//   render() {
+//     return (
+//       <AuthConsumer> 
+//         { auth => 
+//           <Navbar { ...this.props } auth={auth} />
+//         }
+//       </AuthConsumer>
+//     )
+//   }
+// }
+
+    
 
 export class ConnectedNavbar extends React.Component {
   render() {
